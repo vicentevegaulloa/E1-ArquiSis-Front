25c645ba9e41230e359d9de59a15e31be6ba13dc
valeeeriquelme <valeeeriquelme@DESKTOP-G4K82CO>
2023-10-30 12:33:11 -0300

    webpay listo

--- e1-arquisis/src/App.js
+++ e1-arquisis/src/App.js
@@ -10,7 +10,7 @@ import CompanyStocks2 from './pages/Stocks/CompanyStocks2';
 import UserWallet from './pages/UserWallet/UserWallet';
 import PurchasesList2 from './pages/PurchasesList/PurchasesList2';
 import HomePage from './pages/Homepage/Homepage';
-import PurchaseConfirmation from "./pages/Webpay/purchaseConfirmationjsx";
+import PurchaseConfirmation from "./pages/Webpay/PurchaseConfirmation.jsx";
 
 import awsExports from "./aws-exports";
 
@@ -93,7 +93,7 @@ const App = ({ signOut, user }) => {
                       <Route path="/stocks" element={<AllCompanies/>}></Route>
                       <Route path="/wallet" element={<UserWallet user={user} signOut={signOut}/>} />
                       <Route path="/purchases" element={<PurchasesList2 />} />
-                      <Route path="/purchase-confirmation/:token_ws" element={<PurchaseConfirmation />} />                    </Routes>
+                      <Route path="/purchase-confirmation/" element={<PurchaseConfirmation />} />                    </Routes>
                   </div>
                 </div>
               ) : (
